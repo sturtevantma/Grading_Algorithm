68caf720c111d44512b379e59ec2d6c4a4c5f1d2
Dcmg2002 <dean_geraci@uri.edu>
2022-10-19 13:09:49 -0400

    Changed weight and weight_achieved to floats

--- Task.h
+++ Task.h
@@ -4,14 +4,14 @@ class Task{
 
     private:
 
-        unsigned int weight;
-        unsigned int weight_achieved;
+        float weight;
+        float weight_achieved;
         float grade;
 
     public:
 
         std::string name;
         std::string description;
-        Task(std::string name, std::string description, int weight );
+        Task(std::string name, std::string description, float weight );
 
 };
