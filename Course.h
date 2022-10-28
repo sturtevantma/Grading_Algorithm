abe9a809be028dc89ef587a435106a0254034b56
MNewton10 <michael_newton@uri.edu>
2022-10-28 13:40:28 -0400

    Added comments to Course.h

--- Course.h
+++ Course.h
@@ -19,7 +19,10 @@ private:
     float final_Grade;
 
 public:
+    //First type of Course, which has the final grade already with weights taken into account
     Course(float cFinal_Grade);
+
+    //Second type of Course, which takes in the name of the category, the student's average in the category, and the weight of the category
     Course(std::string cName, float cGrade, float cWeight);
 
     float calc_grade();
