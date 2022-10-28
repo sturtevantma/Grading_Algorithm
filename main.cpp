337b3247144aac54bd59c922f54b667a3d8ce522
MNewton10 <98552629+MNewton10@users.noreply.github.com>
2022-10-28 17:41:15 -0400

    Restructured Main

--- main.cpp
+++ main.cpp
@@ -1,67 +1,17 @@
 #include <iostream>
-#include "course.h"
+#include "Course.h"
 #include <string>
 
 int main() {
-    std::vector<float> gradeVec;
-    std::vector<float> weightVec;
-
-    float grades;
 
     //Initialization of the various parts of the course which are weighted differently
-    Course homeWorkGrade("homework", 0.90, 0.25);
-    Course labGrade("lab", 0.85, 0.20);
-    Course examGrade("exam", 0.80, 0.30);
-    Course projectGrade("project", 0.75, 0.25);
+    Course CSC_212("CSC_212", "Class Discussing Data Structures and Algorithms");
 
-    //The next four calls all multiply the weight from a percent to a number so that it can be referenced in multiplying the actual grade value later
-    //These values are pushed onto a vector which is referenced later on
-    //Call 1
-    float hwW = homeWorkGrade.get_num_grade();
-    weightVec.push_back(hwW);
+    float final_grade = CSC_212.calc_grade();
+    std::string letterGrade = CSC_212.calc_grade_letter();
 
-    //Call 2
-    float labW = labGrade.get_num_grade();
-    weightVec.push_back(labW);
-
-    //Call 3
-    float examW = examGrade.get_num_grade();
-    weightVec.push_back(examW);
-
-    //Call 4
-    float projectW = projectGrade.get_num_grade();
-    weightVec.push_back(projectW);
-
-    //The next four calls reference the weight and actually calculates the grade based on the weight
-    //It adds these to a new vector of weighted grades which can be added to provide the true final grade
-
-    //Call 1
-    float hwG = homeWorkGrade.calc_grade();
-    gradeVec.push_back(hwG);
-
-    //Call 2
-    float labG = labGrade.calc_grade();
-    gradeVec.push_back(labG);
-
-    //Call 3
-    float examG = examGrade.calc_grade();
-    gradeVec.push_back(examG);
-
-    //Call 4
-    float projectG = projectGrade.calc_grade();
-    gradeVec.push_back(projectG);
-
-    //This references the weighted portions of the grade, and adds them together to provide the actual final grade with weights taken into account
-    for(int i = 0; i < gradeVec.size(); i++) {
-        grades += gradeVec[i];
-    }
-    //This creates a new Course object using the first constructor in "Course.h" which can be used for getting the letter grade
-    Course finalGrade(grades);
-
-    //This references the new constructed Course and calculates the letter equivalent based on the Syllabus information
-    std::string letterGrade = finalGrade.calc_grade_letter();
-
-    std::cout << letterGrade << " " << grades << std::endl;
+    std::cout << "Number grade: " << final_grade << std::endl;
+    std::cout << "Letter grade: " << letterGrade << std::endl;
 
     return 0;
 }
