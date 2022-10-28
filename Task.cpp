4c1c0789ba03387d62e2e40189b794237a83661a
sturtevantma <sturtevantma21@gmail.com>
2022-10-28 17:10:13 -0400

    Updated Task.cpp to include header changes

--- Task.cpp
+++ Task.cpp
@@ -5,6 +5,5 @@ Task::Task(std::string name, std::string description, float weight ) {
     this->name = name;
     this->description = description;
     this->weight = weight;
-    this->weight_achieved = 0;
     this->grade = 0;
 }
