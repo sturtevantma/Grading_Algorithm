2cea10a18108f964149f70531ff6f065ff1c1ee7
fernans728 <98556788+fernans728@users.noreply.github.com>
2022-10-28 18:52:37 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,24 @@ int main() {
     //Initialization of the various parts of the course which are weighted differently
     Course CSC_212("CSC_212", "Class Discussing Data Structures and Algorithms");
 
-    float final_grade = CSC_212.calc_grade();
-    std::string letterGrade = CSC_212.calc_grade_letter();
-
-    std::cout << "Number grade: " << final_grade << std::endl;
-    std::cout << "Letter grade: " << letterGrade << std::endl;
     CSC_212.add_task("Labs" , "5%", 0.05);
     CSC_212.add_task("Programming Assignments" , "50%", 0.50);
     CSC_212.add_task("Term Project" , "35%", 0.35);
     CSC_212.add_task("Final Exam" , "10%", 0.1);
     CSC_212.add_task("Review Project" , "3%", 0.03);
+
+    CSC_212.add_grade("Labs", 0.90);
+    CSC_212.add_grade("Programming Assignments", 100.0);
+    CSC_212.add_grade("Term Project", 0.85);
+    CSC_212.add_grade("Final Exam", 0.80);
+    CSC_212.add_grade("Review Project", 0.83);
+
+    float final_grade = CSC_212.calc_grade();
+    std::string letterGrade = CSC_212.calc_grade_letter();
+
+
+    std::cout << "Number grade: " << final_grade << std::endl;
+    std::cout << "Letter grade: " << letterGrade << std::endl;
 
     return 0;
 }
